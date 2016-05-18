7494a8ce2765c35543ebfa581ab90c29358d708f
Juan Pablo Giron Ruiz <jpgironruiz@gmail.com>
2016-05-18 02:18:37 +0000

    were deleted several files no have interesting

--- python/terminal_from_.py
+++
@@ -1,4 +0,0 @@
-import os
-f = os.popen('date')
-now = f.read()
-print "Today is ", now
--- python/terminal_from_open_windows.py
+++
@@ -1,9 +0,0 @@
-import os
-
-
-def readFile(filename):
-    filehandle = open(filename)
-    print filehandle.read()
-    filehandle.close()
-
-readFile('hola.txt')
