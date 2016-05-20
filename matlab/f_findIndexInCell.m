313a9a7adc0107a3d6ab2b209c1d14c3f9f396e0
Juan pablo UFRJ <jpgironruiz@gmail.com>
2016-05-20 17:10:07 -0300

    Dont show the index more

--- matlab/f_findIndexInCell.m
+++ matlab/f_findIndexInCell.m
@@ -3,7 +3,7 @@ function index = f_findIndexInCell(desired_signal2plot,vec_signals,len_vector_si
 
 for i=1:len_vector_signals
     if strcmp(desired_signal2plot,vec_signals{i})
-        index = i
+        index = i;
         return
     end
 end
