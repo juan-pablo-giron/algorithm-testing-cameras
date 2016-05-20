54a9eb8e277f46de1e0bf904697e903d51102938
Juan pablo UFRJ <jpgironruiz@gmail.com>
2016-05-20 17:08:02 -0300

    Function calculates the index of the one array CELL string of the desired signal defined by the user

---
+++ matlab/f_findIndexInCell.m
@@ -0,0 +1,9 @@
+function index = f_findIndexInCell(desired_signal2plot,vec_signals,len_vector_signals)
+
+
+for i=1:len_vector_signals
+    if strcmp(desired_signal2plot,vec_signals{i})
+        index = i
+        return
+    end
+end
