44ff9aada546a75bf87435dae19c71d2732deb05
Juan pablo UFRJ <jpgironruiz@gmail.com>
2016-05-24 18:32:41 -0300

    Now work but dont work as function yet

--- matlab/DVS_model_fn.m
+++ matlab/DVS_model_fn.m
@@ -1,12 +1,25 @@
 %% ================================================================ %%
-%   It script is intended to show the response of the DVS model pixel
-%   The model can be found in the papers of Tobi Delbruck and 
-%   Linares-barranco.
+%   The function of this script is calculates the desired behavioural
+%   response of one pixel DVS. The response is based in the model presented
+%   by Tobi Delbruck and Linares-barranco. It script determines the
+%   quantity of pixels with the quantity of columns of the input signal
+%   file as N = len(Columns_file)-1, discard the time.
+%   Input: -full_path of the file input simulated in the circuit in Cadence
+%          -Name of the simulation            
+%   Output: - This script write a .csv file with 2N+1 columns where 2 is be
+%   cause are two signals ON/OFF, N is the total of pixels of the array, 
+%   and the first columns is refer to the the simulation is to one period.
+%   The column are order so: time ON_pix0 OFF_pix0 ON_pix1 OFF_pix1 so on
 %% ================================================================ %%
 
-%function [output] = DVS_model_fn(full_path_input_simulation)
+%function [] = DVS_model_fn(full_path_input_simulation,Name_simulation)
+
+
 
 clear all;clc;close all;
+
+full_path_input_simulation='/home/netware/users/jpgironruiz/Desktop/Documents/Cadence_analysis/Simulation_cameras/SIM2/input_SIM2/input_SIM20.csv';
+Name_simulation='TEEEST';
 
 %% ========================= PARAMAMETERS MOSFET  ================= %%
 nn = 1.334;
@@ -22,118 +35,124 @@ Isn = 2*nn*fi^2*Kn*Ratio;
 %% ========================== MODEL =============================== %%
 
 %% Input signal
-full_path_input_simulation = '/home/netware/users/jpgironruiz/Desktop/Documents/Cadence_analysis/Simulation_cameras/SIM2/input_SIM2/input_SIM20.csv';
-signal2 = importdata(full_path_input_simulation);
 
-t = signal2(:,1);
-Iph = signal2(:,2);
+input_signal = importdata(full_path_input_simulation);
+quant_pixels = length(input_signal(1,:))-1;
+t = input_signal(:,1);
 
-%Iph = Iph2(1:length(Iph2)); % The first element is infinty
-%t = t2(1:length(t2)); % The first element is infinity
+%% Build the vector for the output
 
-
-%% output spectre
-full_path_input_simulation = '/home/netware/users/jpgironruiz/Desktop/Documents/Cadence_analysis/Simulation_cameras/SIM2/output_matlab_SIM2/output_matlab_SIM2.csv';
-signal = importdata(full_path_input_simulation);
-
-t_spectre = signal(:,1);
-C_ON_REQ = signal(:,8);
-C_OFF_REQ = signal(:,7);
-% Take the second period
-index_valid_period0 = find(t_spectre >= 1e-3,1);
-index_valid_period1 = find(t_spectre >= 2e-3,1);
-
-%current_factor = 100e-12;
-%t = 0.1:.00001:10;
-C1 = 400e-15;
-C2 = 20e-15;
-A = C1/C2;
-%b = 0;
-%Iph = current_factor*t;
-%Iph = current_factor*t;
 len_t = length(t);
 output_ON = zeros(1,len_t);
 output_OFF = zeros(1,len_t);
+total_result = zeros(len_t,2*quant_pixels+1);
+total_result(:,1) = t;
 %% Equations
 
 % Known vaiables
 Vref = 1.46;
-V_p = 1.3;  % V_tetha+
-V_n = 1.62;  % V_tetha-
-Vos = 5.42e-3;% Voffset comparador
+V_p = 1.3;          % V_tetha+
+V_n = 1.62;         % V_tetha-
+Vos = 5.42e-3;      % Voffset comparador
+A = 20;             % Gain closed loop differentiator
 
-log_Iph = log(Iph/Iph(1));  
-Vdiff = -nn*fi*A*log_Iph;
-Vdiff_max = max(Vdiff);    %used to normalized
-Vdiff = Vdiff - Vdiff_max; %used to normalized
-Vtemp = Vdiff;
-On_event = V_p - Vref + Vos;
-Off_event = V_n - Vref + Vos;
-mem = 0;
-onesON = 0;
-onesOFF = 0;
-for i=1:len_t
-   value = Vdiff(i);
-   if (value < On_event)
-        
-       output_ON(i) = 1.8;
-       Vdiff(i:len_t) = Vdiff(i:len_t) + abs(value); %reset middle point
-       onesON = onesON + 1;     
-   else
-       if ( value > Off_event)
-            
-            output_OFF(i) = 1.8;
-            Vdiff(i:len_t) = Vdiff(i:len_t) - abs(value); %reset middle point
-            onesOFF = onesOFF + 1;  
-       else
-           continue
-       end
-   end
+VdiffON = V_p - Vref + Vos;  
+VdiffOFF= V_n - Vref + Vos;
+
+% it loop simulated the behaviour of the model on the time.
+
+for N=1:quant_pixels
     
+    Iph = input_signal(:,N+1);
+    log_Iph = log(Iph/Isn);  
+    Vdiff = -nn*fi*A*log_Iph;
+    Vdiff_max = max(Vdiff);    %used to normalized
+    Vdiff = Vdiff - Vdiff_max; %used to normalized
+    
+    
+    for i=1:len_t
+       value = Vdiff(i);
+       if (value <= VdiffON)
+
+           output_ON(i) = 1.8;
+           Vdiff(i:len_t) = Vdiff(i:len_t) + abs(value); %reset to Vref
+
+       else
+           if ( value >= VdiffOFF)
+
+                output_OFF(i) = 1.8;
+                Vdiff(i:len_t) = Vdiff(i:len_t) - abs(value); %reset to Vref
+
+           else
+               continue
+           end
+       end
+
+    end
+    total_result(:,N+1) = output_ON';
+    total_result(:,N+2) = output_OFF';
+    
+
 end
-onesON
-onesOFF
 % Setting the Vref as the plot in spectre
-Vdiff = Vdiff + Vref;
+%Vdiff = Vdiff + Vref;
+
+%% Write the file a .csv file
+
+dlmwrite(strcat('Expected_behaviour_',Name_simulation,'.csv'),total_result, ...
+        'delimiter',' ','precision',10,'newline','unix');
+    
+
+%% plot
+
+subplot(2,1,1)
+plot(t,total_result(:,2))
+ylabel('ON EVENTS')
+xlabel('time')
+
+subplot(2,1,2)
+plot(t,total_result(:,3))
+ylabel('OFF EVENTS')
+xlabel('time')
 
 %% plots
 
 
-h = figure(1);
-
-t0 = 1e-3;
-
-subplot(321)
-stem((t+t0),output_ON)
-xlabel('Tiempo')
-ylabel('ON Events expected')
-
-subplot(323)
-%plot(t,Vdiff)
-plot(t_spectre([index_valid_period0:index_valid_period1]), ...
-    C_ON_REQ([index_valid_period0:index_valid_period1]),'r')
-xlabel('Tiempo')
-ylabel('ON Events From Pixel simulated')
-
-subplot(322)
-stem((t+t0),output_OFF)
-xlabel('Tiempo')
-ylabel('OFF Events expected')
-
-subplot(324)
-%plot(t,Vdiff)
-plot(t_spectre([index_valid_period0:index_valid_period1]), ...
-    C_OFF_REQ([index_valid_period0:index_valid_period1]),'r')
-xlabel('Tiempo')
-ylabel('OFF Events From Pixel simulated')
-
-
-subplot(3,2,[5,6])
-%plot(t,Vdiff)
-
-semilogy(t(1:length(t)-1)+t0,abs(nn*fi*(-A)*diff(log(Iph/Isn))))
-xlabel('Tiempo')
-ylabel('-A*n*\phi_{t}*Log(Iph/Isn)')
+% h = figure(1);
+% 
+% t0 = 1e-3;
+% 
+% subplot(321)
+% stem((t+t0),output_ON)
+% xlabel('Tiempo')
+% ylabel('ON Events expected')
+% 
+% subplot(323)
+% %plot(t,Vdiff)
+% plot(t_spectre([index_valid_period0:index_valid_period1]), ...
+%     C_ON_REQ([index_valid_period0:index_valid_period1]),'r')
+% xlabel('Tiempo')
+% ylabel('ON Events From Pixel simulated')
+% 
+% subplot(322)
+% stem((t+t0),output_OFF)
+% xlabel('Tiempo')
+% ylabel('OFF Events expected')
+% 
+% subplot(324)
+% %plot(t,Vdiff)
+% plot(t_spectre([index_valid_period0:index_valid_period1]), ...
+%     C_OFF_REQ([index_valid_period0:index_valid_period1]),'r')
+% xlabel('Tiempo')
+% ylabel('OFF Events From Pixel simulated')
+% 
+% 
+% subplot(3,2,[5,6])
+% %plot(t,Vdiff)
+% 
+% semilogy(t(1:length(t)-1)+t0,abs(nn*fi*(-A)*diff(log(Iph/Isn))))
+% xlabel('Tiempo')
+% ylabel('-A*n*\phi_{t}*Log(Iph/Isn)')
 
 %saveas(h,'Plot2.png','png')
 
