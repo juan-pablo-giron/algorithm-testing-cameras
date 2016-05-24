0991f3275506e60b229118187f39ea32cc72e309
Juan pablo UFRJ <jpgironruiz@gmail.com>
2016-05-24 18:55:16 -0300

    now the function read the quantity of files in the filder input and acknowledge the quant_pixels.. now work

--- matlab/DVS_model_fn.m
+++ matlab/DVS_model_fn.m
@@ -2,9 +2,8 @@
 %   The function of this script is calculates the desired behavioural
 %   response of one pixel DVS. The response is based in the model presented
 %   by Tobi Delbruck and Linares-barranco. It script determines the
-%   quantity of pixels with the quantity of columns of the input signal
-%   file as N = len(Columns_file)-1, discard the time.
-%   Input: -full_path of the file input simulated in the circuit in Cadence
+%   quantity of pixels with the quantity of files in the input PATH.
+%   Input: -full path of the file input simulated in the circuit in Cadence
 %          -Name of the simulation            
 %   Output: - This script write a .csv file with 2N+1 columns where 2 is be
 %   cause are two signals ON/OFF, N is the total of pixels of the array, 
@@ -12,14 +11,17 @@
 %   The column are order so: time ON_pix0 OFF_pix0 ON_pix1 OFF_pix1 so on
 %% ================================================================ %%
 
-%function [] = DVS_model_fn(full_path_input_simulation,Name_simulation)
+%function [] = DVS_model_fn(path_input_simulation,path_output,Name_simulation)
 
 
 
 clear all;clc;close all;
 
-full_path_input_simulation='/home/netware/users/jpgironruiz/Desktop/Documents/Cadence_analysis/Simulation_cameras/SIM2/input_SIM2/input_SIM20.csv';
-Name_simulation='TEEEST';
+path_input_simulation='/home/netware/users/jpgironruiz/Desktop/Documents/Cadence_analysis/Simulation_cameras/SIM2/input_SIM2/';
+path_output = '/home/netware/users/jpgironruiz/Desktop/Documents/Cadence_analysis/Simulation_cameras/SIM2/input_SIM2/';
+Name_simulation='SIM2';
+D = dir([path_input_simulation, '/*.csv']);
+Num = length(D(not([D.isdir]))); %Count how files there are.
 
 %% ========================= PARAMAMETERS MOSFET  ================= %%
 nn = 1.334;
@@ -34,19 +36,22 @@ Isn = 2*nn*fi^2*Kn*Ratio;
 
 %% ========================== MODEL =============================== %%
 
-%% Input signal
+quant_pixels = Num;
 
-input_signal = importdata(full_path_input_simulation);
-quant_pixels = length(input_signal(1,:))-1;
-t = input_signal(:,1);
 
 %% Build the vector for the output
+% here is read the time that is equal to others input. At least there is
+% one pixel
 
-len_t = length(t);
+name_input = strcat(path_input_simulation,'input_',Name_simulation,int2str(0),'.csv');
+input_signal = importdata(name_input);
+t = input_signal(:,1);
+len_t = length(t);  
 output_ON = zeros(1,len_t);
 output_OFF = zeros(1,len_t);
 total_result = zeros(len_t,2*quant_pixels+1);
 total_result(:,1) = t;
+
 %% Equations
 
 % Known vaiables
@@ -62,8 +67,9 @@ VdiffOFF= V_n - Vref + Vos;
 % it loop simulated the behaviour of the model on the time.
 
 for N=1:quant_pixels
-    
-    Iph = input_signal(:,N+1);
+    name_input = strcat(path_input_simulation,'input_',Name_simulation,int2str(N-1),'.csv');
+    input_signal = importdata(name_input);
+    Iph = input_signal(:,2);
     log_Iph = log(Iph/Isn);  
     Vdiff = -nn*fi*A*log_Iph;
     Vdiff_max = max(Vdiff);    %used to normalized
@@ -98,11 +104,10 @@ end
 %Vdiff = Vdiff + Vref;
 
 %% Write the file a .csv file
-
+cd(path_output)
 dlmwrite(strcat('Expected_behaviour_',Name_simulation,'.csv'),total_result, ...
         'delimiter',' ','precision',10,'newline','unix');
     
-
 %% plot
 
 subplot(2,1,1)
@@ -115,48 +120,4 @@ plot(t,total_result(:,3))
 ylabel('OFF EVENTS')
 xlabel('time')
 
-%% plots
 
-
-% h = figure(1);
-% 
-% t0 = 1e-3;
-% 
-% subplot(321)
-% stem((t+t0),output_ON)
-% xlabel('Tiempo')
-% ylabel('ON Events expected')
-% 
-% subplot(323)
-% %plot(t,Vdiff)
-% plot(t_spectre([index_valid_period0:index_valid_period1]), ...
-%     C_ON_REQ([index_valid_period0:index_valid_period1]),'r')
-% xlabel('Tiempo')
-% ylabel('ON Events From Pixel simulated')
-% 
-% subplot(322)
-% stem((t+t0),output_OFF)
-% xlabel('Tiempo')
-% ylabel('OFF Events expected')
-% 
-% subplot(324)
-% %plot(t,Vdiff)
-% plot(t_spectre([index_valid_period0:index_valid_period1]), ...
-%     C_OFF_REQ([index_valid_period0:index_valid_period1]),'r')
-% xlabel('Tiempo')
-% ylabel('OFF Events From Pixel simulated')
-% 
-% 
-% subplot(3,2,[5,6])
-% %plot(t,Vdiff)
-% 
-% semilogy(t(1:length(t)-1)+t0,abs(nn*fi*(-A)*diff(log(Iph/Isn))))
-% xlabel('Tiempo')
-% ylabel('-A*n*\phi_{t}*Log(Iph/Isn)')
-
-%saveas(h,'Plot2.png','png')
-
-%% ================================================================ %%
-
-
-%% ================================================================ %%
