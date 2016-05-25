a7e91b6111e31249299e087404360201fcda77af
Juan pablo UFRJ <jpgironruiz@gmail.com>
2016-05-25 14:18:22 -0300

    Was added the algorithm to plot for one only pixel

---
+++ matlab/plot_signal_unique_pixel.m
@@ -0,0 +1,14 @@
+%% ================================================================ %%
+%   
+%   Function: It algortihm does the plot of the signals that correspond
+%   to the ON/OFF event both to the model as the obtained by simulation.
+%   It function is used when the simulation is for one unique pixel.
+%   
+%   Input: - PATH_OUTPUT_SIMULATION
+%                     
+%   Output:- Create a .Fig and .png graphic with the respective axis la- 
+%   bel.
+%  
+%   
+%% ================================================================ %%
+
