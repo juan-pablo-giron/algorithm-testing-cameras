5f1040833587e8d146779a8b8db6462f0291d6c6
Juan Pablo Giron Ruiz <jpgironruiz@gmail.com>
2016-05-26 17:35:55 +0000

    does the plot

--- matlab/simulation_spectre_pixel_UNIX.m
+++ matlab/simulation_spectre_pixel_UNIX.m
@@ -118,21 +118,21 @@ status=system(command)
 
 
 	%Plot the signals
-%	desired_signal2plot = 'C_ON_REQ';
-%	Index_ON =  %f_findIndexInCell(desired_signal2plot,vec_signals,len_vector_signals);
-%	Index_ON = Index_ON + 1;
+desired_signal2plot = 'C_ON_REQ';
+Index_ON =  f_findIndexInCell(desired_signal2plot,vec_signals,len_vector_signals);
+Index_ON = Index_ON + 1;
 
-%	desired_signal2plot = 'C_OFF_REQ';
-%	Index_OFF =  %f_findIndexInCell(desired_signal2plot,vec_signals,len_vector_signals);
-%	Index_OFF = Index_OFF + 1;
+desired_signal2plot = 'C_OFF_REQ';
+Index_OFF = f_findIndexInCell(desired_signal2plot,vec_signals,len_vector_signals);
+Index_OFF = Index_OFF + 1;
 
-%	time_start = 1e-3;
-%	time_stop = 2e-3;
+time_start = 1e-3;
+time_stop = 2e-3;
 
-%	plot_signal_unique_pixel(PATH_sim_output_matlab,...
-%	PATH_folder_input,Index_ON,Index_OFF,time_start,time_stop)
+plot_signal_unique_pixel(PATH_sim_output_matlab,...
+PATH_folder_input,Index_ON,Index_OFF,time_start,time_stop)
 
-%	cd(PATH_scriptMatlab)
+cd(PATH_scriptMatlab)
 
 %	exit
 	
@@ -151,3 +151,4 @@ system(command)
 
 display('END_SIMULATION')
 
+exit
