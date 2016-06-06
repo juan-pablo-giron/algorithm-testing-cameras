a7a070b7a20e1c3b22424932a8efa15ef862d3c7
Juan pablo UFRJ <jpgironruiz@gmail.com>
2016-06-06 13:30:45 -0300

    Is created the script to plot 3D graphic from simulation data to the pixel DVS

---
+++ python/plot3D_DVS.py
@@ -0,0 +1,7 @@
+## --------------------------------------------------- ##
+## This algorithm does the plot in 3D of the transient
+## response retuned by Spectre. It algorithm is designed
+## to work concurrently with the transient simulation
+## minimizing the time of processing and given the plot
+## at the same time of the end of the simulation.
+## Designed by Juan Pablo Giron jpgironruiz@gmail.com
