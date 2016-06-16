9a4131ad1c4c7337e8864c375c9e4656582fa777
Juan pablo UFRJ <jpgironruiz@gmail.com>
2016-06-16 15:38:53 -0300

    se crea un script que permita obtener los indeces de las senales deseadas

---
+++ python/getIndex_desiredSignals.py
@@ -0,0 +1,5 @@
+## Esta function recibe una lista con las senhales simuladas
+## Ademas de la cantidad de pixels y otra lista con las senhales deseadas
+## El retorno de la funcion es escribir un archivo .csv indicando las
+## posiciones de las senhales deseadas.
+
