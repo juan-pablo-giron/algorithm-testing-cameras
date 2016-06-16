f2412d2f03bc6735f8c7adae312d154d0f8c17b5
Juan pablo UFRJ <jpgironruiz@gmail.com>
2016-06-16 16:32:40 -0300

    es creada una funcion en Matlab que plotea la respuesta tran del pixel DVS

---
+++ matlab/plotTran_DVS.m
@@ -0,0 +1,7 @@
+% Este script plotea la salida de una camara DVS 
+
+
+
+PATH_sim_output_matlab = '/home/netware/users/jpgironruiz/Desktop/Documents/Cadence_analysis/Simulation_cameras/DVS2x2_X_Arbiter_TS_20res/output_matlab/';
+cd(PATH_sim_output_matlab)
+
