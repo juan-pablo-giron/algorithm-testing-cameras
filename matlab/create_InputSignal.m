923761a338e9aa28d9e47bf06ad34cc918584c19
Juan pablo UFRJ <jpgironruiz@gmail.com>
2016-06-20 19:16:02 -0300

    working

--- matlab/create_InputSignal.m
+++ matlab/create_InputSignal.m
@@ -1,21 +1,20 @@
 function []=create_InputSignal(time,delta_time,N,M,samples,Array,nameSignal,PATH_input)
 
+curr_pwd = pwd;
 cd(PATH_input)
-name_folder = nameSignal;
-cd(name_folder)
 pixel = 0;
 for ind_y=1:M
     for ind_x=1:N
         % Here is sampled the signal the by 'samples' times.
-        vec_time = time:delta_time/samples:delta_time-delta_time/samples;
+        vec_time = time:delta_time/samples:delta_time-delta_time/samples + time;
         value_pixel = Array(ind_y,ind_x);
         vec_value = value_pixel*ones(1,length(vec_time));
-        name_file = strcat(nameSignal,pixel,'.csv');
+        name_file = strcat(nameSignal,int2str(pixel),'.csv');
         dlmwrite(name_file,[vec_time' vec_value'],'delimiter',' ','-append','precision',10,'newline','unix');
         pixel = pixel + 1;
     end
    
 end
 
-
+cd(curr_pwd)
     
