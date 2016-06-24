4283f5f39965ffd631497bcab2fe164f4103c1ef
Juan pablo UFRJ <jpgironruiz@gmail.com>
2016-06-24 10:57:50 -0300

    several mistakes solved

--- matlab/post_processingNO_GUI.sh
+++ matlab/post_processingNO_GUI.sh
@@ -1,29 +1,30 @@
 #!/bin/bash
 
 cd ../../
-kdialog --msgbox "select the env_variables of the simulation 
- that you want doing post-processing"
-PATH_env_var=$(kdialog --getopenfilename "env_var.sh"  "*.sh ");
-if [ "$?" -ne 0 ]; then
-  kdialog --error "Post-processing Aborted"
+
+PATH_env_var=$(dialog --stdout --title "Choose an env_var file" --fselect $PWD/ 10 10)
+if [ "$?" -ne 0 ]
+then
+  #log --error "Post-processing Aborted"
   return
-fi;
+fi
 
 source $PATH_env_var
 
 #executing the commands for post-processing
 
-cd $PATH_scriptPython
-python sort_data_DVS_pixel_UNIX.py
+#cd $PATH_scriptPython
+#python sort_data_DVS_pixel_UNIX.py
   
-if [ "$?" = 0 ]
-then
+#if [ "$?" = 0 ]
+#then
   #kdialog --msgbox "Starting the Post-processing with MATLAB close it when end"
-  cd $PATH_scriptMatlab
-  echo $PATH_scriptMatlab
-  matlab -nodesktop -nosplash -r plotTran_DVS
-  kdialog --msgbox "Post-processing with successful check the images"
-else
-  kdialog --error "PLEASE verify your Python script there are some errors
-  run post-processing after solved"
-fi
+cd $PATH_scriptMatlab
+echo $PATH_scriptMatlab
+matlab -nodesktop -nosplash -r plotTran_DVS
+#  dialog --msgbox "Post-processing with successful check the images"
+#else
+#  kdialog --error "PLEASE verify your Python script there are some errors
+#  run post-processing after solved"
+#fi
+
